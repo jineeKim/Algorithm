74937129e09f421d3119f63c4c377a30ea193d96
jineeKim <jin03hee16@gmail.com>
2019-08-09 00:25:44 +0900

    0808

--- 2019_08/match.cpp
+++ 2019_08/match.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+//(b-c)+((((((a+b)+c)+d)+e)+f)+g)+(e*g)+h+)
+
 int main() {
 	char arr[50] = { 0, };
 	char out[50] = { 0, };
@@ -11,16 +13,17 @@ int main() {
 
 	for (int i = 0; i < strlen(arr); i++) {
 		if (arr[i] == '(') {
-			out[++j] = i;
-		}
-		else if (arr[i] == ')') {
-			printf("%d %d\n", out[j], i);
-			if (j > 1)
-				j--;
-		}
-		else if (j < 1 ) {
+			out[j++] = i;
+		}else if ((j == 0 && arr[i] == '(')) {
 			printf("not match\n");
 		}
+		else if (j == 0 && arr[i] == ')') {
+			printf("not match\n");
+		}
+		else if (arr[i] == ')') {
+			printf("%d %d\n", out[--j], i);
+		}
+		
 	}
 
 	return 0;
