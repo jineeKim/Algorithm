ac0fabce2ec597760f18148304f6dd9ac273fc41
jineeKim <jin03hee16@gmail.com>
2019-08-20 16:45:55 +0900

    0820

---
+++ 2019_08/binary_gap.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int solution(int N) {
+	int s = 0, f = 0;
+	int max = 0;
+	int i = 0;
+
+	for (i = 0; N != 0; i++) {
+		if (N % 2) {
+			if (max <= i - s) {
+				max = i - s;
+			}
+			s = i;
+			f++;
+		}
+		N /= 2;
+	}
+
+	if (f == 1)
+		return  0;
+	else
+		return max - 1;
+}
+int main() {
+	int n = 0;
+
+	scanf("%d", &n);
+	printf("%d", solution(n));
+
+}
