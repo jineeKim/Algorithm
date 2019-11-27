0489069218cbd7d039a12d6ac66c37380236fc4f
jineeKim <jin03hee16@gmail.com>
2019-11-27 23:13:34 +0800

    1127

---
+++ 2019_11/watermelon.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	for (int i = 1; i <= n; i++) {
+		if (i % 2)
+			answer += "¼ö";
+		else
+			answer += "¹Ú";
+	}
+	return answer;
+}
+
+int main() {
+	solution(3);
+	return 0;
+}
